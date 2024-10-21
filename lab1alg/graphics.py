1129521869e23082efbee2432f3091cb34481f7f
Kirill Faber <KIRILLFABERR@yandex.ru>
2024-10-21 23:35:38 +0300

    Graphics

--- lab1alg/graphics.py
+++ lab1alg/graphics.py
@@ -2,50 +2,291 @@ import csv
 import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
+from sympy import *
 
-n = []
-T = []
+FROM = 10000
+TO = 100000
+
+def reg(n, T, col):
+    # Находим коэффициенты полиномиальной регрессии 2-й степени
+    coefficients = np.polyfit(n, T, 2)  # 2 означает полином второй степени
+    polynomial_regression = np.poly1d(coefficients)
+
+    # Генерация значений для кривой регрессии
+    x_reg = np.linspace(min(n), max(n), 100)
+    y_reg = polynomial_regression(x_reg)
+
+    # Построение кривой регрессии
+    plt.plot(x_reg, y_reg, color=col)
+
+
+def printGraphics(data_n, data_T, title, filename):
+    plt.clf()
+    # Построение графика с точками
+    plt.scatter(data_n[0], data_T[0], label='Sorted')
+    plt.scatter(data_n[1], data_T[1], label='Semi-Sorted')
+    plt.scatter(data_n[2], data_T[2], label='Unsorted')
+    plt.scatter(data_n[3], data_T[3], label='Random fill')
+
+    reg(data_n[0], data_T[0], 'blue')
+    reg(data_n[1], data_T[1], 'orange')
+    reg(data_n[2], data_T[2], 'green')
+    reg(data_n[3], data_T[3], 'red')
+
+    # Добавление подписей к осям
+    plt.xlabel('n')
+    plt.ylabel('T(n)')
+
+    # Заголовок графика
+    plt.title(title)
+
+    # Легенда
+    plt.legend()
+
+    # Отображение графика
+    plt.savefig(filename)
+    #plt.show()
+
+
+    
+            
+
+def readData(n, T, sort, fill):
+    n.clear()
+    T.clear()
+    with open("DATA.csv", "r") as r_file:
+        reader = csv.DictReader(r_file, delimiter=";")
+        for row in reader:
+            if(row["sort"] == sort and row["fill"] == fill):
+                n.append(int(row["n"]))
+                T.append(float(row["T(n)"]))
+    
+                
+def clearData():
+    global n_s, T_s, n_ss, T_ss, n_us, T_us, n_rs, T_rs
+    n_s.clear()
+    T_s.clear()
+    n_ss.clear()
+    T_ss.clear()
+    n_us.clear()
+    T_us.clear()
+    n_rs.clear()
+    T_rs.clear()
+
+# Графики для каждой сортировки (каждое заполнение)
+def plotSort(sort, name, filename):
+    clearData()  # Очищаем данные перед новой отрисовкой
+    readData(n_s, T_s, sort, "S")
+    readData(n_ss, T_ss, sort, "SS")
+    readData(n_us, T_us, sort, "US")
+    readData(n_rs, T_rs, sort, "RS")
+    data_n = [n_s, n_ss, n_us, n_rs]
+    data_T = [T_s, T_ss, T_us, T_rs]
+    printGraphics(data_n, data_T, name, filename)
 
 
 
-# Чтение данных из CSV файла
-with open("DATA.csv", "r") as f:
-    reader = csv.DictReader(f, delimiter=';')
-    for row in reader:
+
+# Графики для теории
+n = Symbol('n')
+
+# Графики для каждой сортировки (все случаи на одном изображении)
+# Selection Sort
+title = "Selection Sort"
+T_w = 5 * n ** 2 + 3 * n + 1
+T_b = 3 * n ** 2 + 4 * n + 1
+T_a = 4 * n ** 2 + 7 * n + 1
+plot1 = plot(T_w, (n, FROM, TO), show = False, label = "Worst case", xlabel = "n", ylabel = "T(n)", title = title)
+plot2 = plot(T_b, (n, FROM, TO), show = False, label = "Best case")
+plot3 = plot(T_a, (n, FROM, TO), show = False, label = "Average case")
+
+plot1.extend(plot2)
+plot1.extend(plot3)
+plot1.legend = True
+
+
+
+plot1.save('TheorGraphics\SS.png')
+
+# Insertion Sort
+title = "Insertion Sort"
+
+T_w = 3 * n ** 2 + 7 * n + 1
+T_b = 6 * n + 1
+T_a = 3 * n ** 2 + 4 * n + 1
+plot1 = plot(T_w, (n, FROM, TO), show = False, label = "Worst case", xlabel = "n", ylabel = "T(n)", title = title)
+plot2 = plot(T_b, (n, FROM, TO), show = False, label = "Best case")
+plot3 = plot(T_a, (n, FROM, TO), show = False, label = "Average case")
+
+plot1.extend(plot2)
+plot1.extend(plot3)
+plot1.legend = True
+
+
+
+plot1.save('TheorGraphics\IS.png')
+# Bubble Sort
+title = "Bubble Sort"
+
+T_w = 7 * n ** 2 - 10 * n + 5
+T_b = 3 * n + 3
+T_a = 5 * n ** 2 - n - 2
+plot1 = plot(T_w, (n, FROM, TO), show = False, label = "Worst case", xlabel = "n", ylabel = "T(n)", title = title)
+plot2 = plot(T_b, (n, FROM, TO), show = False, label = "Best case")
+plot3 = plot(T_a, (n, FROM, TO), show = False, label = "Average case")
+
+plot1.extend(plot2)
+plot1.extend(plot3)
+plot1.legend = True
+
+
+
+plot1.save('TheorGraphics\BS.png')
+# Merge Sort
+title = "Merge Sort"
+
+T_w = 2 * n + log(n, 2) * (6 * n + 5)
+T_b = 2 * n + log(n, 2) * (6 * n + 5)
+T_a = 2 * n + log(n, 2) * (6 * n + 5) ###
+plot1 = plot(T_w, (n, FROM, TO), show = False, label = "Worst case", xlabel = "n", ylabel = "T(n)", title = title)
+plot2 = plot(T_b, (n, FROM, TO), show = False, label = "Best case")
+plot3 = plot(T_a, (n, FROM, TO), show = False, label = "Average case")
+
+plot1.extend(plot2)
+plot1.extend(plot3)
+plot1.legend = True
+
+
+
+plot1.save('TheorGraphics\MS.png')
+# Shell Sort
+title = "Shell Sort"
+
+T_w = 5 * n ** 2 + 3 * n + 1
+T_b = 3 * n ** 2 + 4 * n + 1
+T_a = 3 * n ** 2 + 4 * n + 1 ###
+plot1 = plot(T_w, (n, FROM, TO), show = False, label = "Worst case", xlabel = "n", ylabel = "T(n)", title = title)
+plot2 = plot(T_b, (n, FROM, TO), show = False, label = "Best case")
+plot3 = plot(T_a, (n, FROM, TO), show = False, label = "Average case")
+
+plot1.extend(plot2)
+plot1.extend(plot3)
+plot1.legend = True
+
+
+
+plot1.save('TheorGraphics\SHS.png')
+# Quick Sort
+title = "Quick Sort"
+
+T_w = n ** 2 + 2 * n
+T_b = n + 2 * n * log(n, 2) - log(n, 2)
+T_a = 3 * n ** 2 + 4 * n + 1 ###
+plot1 = plot(T_w, (n, FROM, TO), show = False, label = "Worst case", xlabel = "n", ylabel = "T(n)", title = title)
+plot2 = plot(T_b, (n, FROM, TO), show = False, label = "Best case")
+plot3 = plot(T_a, (n, FROM, TO), show = False, label = "Average case")
+
+plot1.extend(plot2)
+plot1.extend(plot3)
+plot1.legend = True
+
+
+
+plot1.save('TheorGraphics\QS.png')
+# Heap Sort
+title = "Heap Sort"
+T_w = 5 * n ** 2 + 3 * n + 1
+T_b = 3 * n ** 2 + 4 * n + 1
+T_a = 3 * n ** 2 + 4 * n + 1 ###
+plot1 = plot(T_w, (n, FROM, TO), show = False, label = "Worst case", xlabel = "n", ylabel = "T(n)", title = title)
+plot2 = plot(T_b, (n, FROM, TO), show = False, label = "Best case")
+plot3 = plot(T_a, (n, FROM, TO), show = False, label = "Average case")
+
+plot1.extend(plot2)
+plot1.extend(plot3)
+plot1.legend = True
+
+
+
+plot1.save('TheorGraphics\HS.png')
+
+
+# Графики для каждой сортировки (все случаи на разных изображениях)
+
+
+# График всех сортировок в среднем случае
+title = "Sorts (Average case)"
+T_1 = 1
+T_2 = 2
+T_3 = 3
+T_4 = 4
+T_5 = 5
+T_6 = 6
+T_7 = 7
+
+plot1 = plot(T_1, (n, FROM, TO), show = False, label = "Selection sort", xlabel = "n", ylabel = "T(n)", title = title)
+plot2 = plot(T_2, (n, FROM, TO), show = False, label = "Insertion Sort")
+plot3 = plot(T_3, (n, FROM, TO), show = False, label = "Bubble Sort")
+plot4 = plot(T_4, (n, FROM, TO), show = False, label = "Merge Sort")
+plot5 = plot(T_5, (n, FROM, TO), show = False, label = "Shell Sort")
+plot6 = plot(T_6, (n, FROM, TO), show = False, label = "Quick Sort")
+plot7 = plot(T_7, (n, FROM, TO), show = False, label = "Heap Sort")
+
+plot1.extend(plot2)
+plot1.extend(plot3)
+plot1.extend(plot4)
+plot1.extend(plot5)
+plot1.extend(plot6)
+plot1.extend(plot7)
+plot1.legend = True
+
+
+
+plot1.save('TheorGraphics\ALL.png')
+
+
+# Графики для практики
+
+
+
+# Графики для каждой сортировки (каждое заполнение)
+n_s = []
+T_s = []
+n_ss = []
+T_ss = []
+n_us = []
+T_us = []
+n_rs = []
+T_rs = []
+
+plotSort("SS", "Selection Sort", "PracGraphics\SS.png")
+plotSort("IS", "Insertion Sort", "PracGraphics\IS.png")
+plotSort("BS", "Bubble Sort", "PracGraphics\BS.png")
+plotSort("MS", "Merge Sort", "PracGraphics\MS.png")
+plotSort("SHS", "Shell Sort", "PracGraphics\SHS.png")
+plotSort("QS", "Quick Sort", "PracGraphics\QS.png")
+plotSort("HS", "Heap Sort", "PracGraphics\HS.png")
+
+
+
+
+    
+
+
+
+
+# Графики для каждой сортировки отедльно по типу заполнения
+
+    
+
+
         
-        n.append(int(row['n']))
-        T.append(int(row[' T(n)']))
+        
 
         
-# Построение графика с точками
-plt.scatter(n, T, label='Точки данных')
-
-# Находим коэффициенты полиномиальной регрессии 2-й степени
-coefficients = np.polyfit(n, T, 2)  # 2 означает полином второй степени
-polynomial_regression = np.poly1d(coefficients)
-
-# Генерация значений для кривой регрессии
-x_reg = np.linspace(min(n), max(n), 100)
-y_reg = polynomial_regression(x_reg)
-
-# Построение кривой регрессии
-plt.plot(x_reg, y_reg, color='red', label='Полиномиальная регрессия (2-й степени)')
-
-# Добавление подписей к осям
-plt.xlabel('n')
-plt.ylabel('T(n)')
-
-# Заголовок графика
-plt.title('График с полиномиальной регрессией')
-
-# Легенда
-plt.legend()
-
-# Отображение графика
-plt.show()
 
 
-print(n)
-print(T)
+
+
 
 
--- lab1alg/main.cpp
+++ lab1alg/main.cpp
@@ -3,11 +3,12 @@
 #include <ctime>
 #include <fstream>
 #include <string>
+#include <chrono>
 
 using namespace std;
 
 const int RANGE = 50000;
-const int FROM = 1, TO = 1000, STEP = 100;
+const int FROM = 10000, TO = 100000, STEP = 10000;
 
 
 
@@ -20,21 +21,9 @@ void bubbleSort(vector<int>& arr);
 vector<int> merge(vector<int> left, vector<int> right);
 vector<int> mergeSort(vector<int> arr);
 void shellSort(vector<int>& arr);
+void shellSortHibb(vector<int>& arr);
+void shellSortPratt(vector<int>& arr);
 void quickSort(vector<int>& arr, int start, int end);
-
-
-void selectionSort(vector<int>& arr, int& T);
-void insertionSort(vector<int>& arr, int& T);
-int findMin(vector<int>& arr, int a, int& T);
-void bubbleSort(vector<int>& arr, int& T);
-vector<int> merge(vector<int> left, vector<int> right, int& T);
-vector<int> mergeSort(vector<int> arr, int& T);
-void shellSort(vector<int>& arr, int& T);
-void quickSort(vector<int>& arr, int start, int end, int& T);
-void heapSort(vector<int>& arr, int& T);
-
-
-
 
 
 void printArr(vector<int>& arr);
@@ -140,6 +129,20 @@ vector<int> mergeSort(vector<int> arr) {
 }
 
 void shellSort(vector<int>& arr) {
+    for (int s = arr.size() / 2; s > 0; s /= 2) {
+        for (int i = s; i < arr.size(); i++) {
+            for (int j = i - s; j >= 0 && arr[j] > arr[j + s]; j -= s) {
+                int temp = arr[j];
+                arr[j] = arr[j + s];
+                arr[j + s] = temp;
+            }
+        }
+    }
+}
+void shellSortHibb(vector<int>& arr) {
+
+}
+void shellSortPratt(vector<int>& arr) {
 
 }
 
@@ -221,183 +224,6 @@ void heapSort(vector<int>& arr) {
 
 
 
-// Перегрузки сортировок для подсчета операций
-
-
-int findMin(std::vector<int>& arr, int a, int& T) {
-    int min = arr[a], min_index = a;
-    T += 3; // Инициализация
-    for (int i = a + 1; i < arr.size(); i++) {
-        T += 2; // Сравнения
-        if (min > arr[i]) {
-            min = arr[i];
-            min_index = i;
-            T += 2;
-        }
-        T++; // i++
-    }
-    T++; // Последнее сравнение
-    T++; // return
-    return min_index;
-}
-
-void selectionSort(std::vector<int>& arr, int& T) {
-    T++; // Инициализация i
-    for (int i = 0; i < arr.size(); i++) {
-        T++; // сравнение
-        int j = findMin(arr, i, T); // Найти индекс минимального элемента
-        T++; // Инициализация j
-        // Обмен значений
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
-        T += 3; // Обмен значений
-        T++; // i++
-    }
-    T++; // Последнее сравнение
-}
-
-void insertionSort(std::vector<int>& arr, int& T) {
-    T++; // Инициализация
-    for (int i = 1; i < arr.size(); i++) {
-        T++; // Сравнение
-        int el = arr[i];  // Сохраняем текущий элемент
-        T++; // Инициализация
-
-        int j = i;
-        T++; // Иницилизация
-        while (j > 0 && arr[j - 1] > el) {
-            T += 2;  // Сравнение
-            arr[j] = arr[j - 1];  // Сдвиг элемента
-            T += 1;  // Операция присвоения
-            j--;
-            T++;  // j--
-        }
-        T++; // Последнее сравнение
-        arr[j] = el;  // Вставка элемента
-        T++;  // Операция присвоения
-    }
-    T++; // Последнее сравнение
-}
-
-void bubbleSort(std::vector<int>& arr, int& T) {
-
-    bool swapped = true;
-    T += 2; // Инициализация
-    for (int i = 0; i < arr.size() - 1 && swapped; i++) {
-        T += 2; // Сравнение
-        swapped = false;
-        T += 2;
-        for (int j = 0; j < arr.size() - i - 1; j++) {
-            T += 2; // Сравнение
-            if (arr[j] > arr[j + 1]) {
-                int tmp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = tmp;
-                swapped = true;
-                T += 4;
-            }
-            T++; //j++
-        }
-        T++; // Последнее сравнение
-        T++; // i++
-    }
-    T++; // Последнее сравнение
-
-}
-
-
-// Функция для слияния двух отсортированных массивов
-std::vector<int> merge(std::vector<int> left, std::vector<int> right, int& T) {
-    // Создаем массив для хранения результата с размером суммы размеров двух входящих массивов
-    std::vector<int> result((left.size() + right.size()));
-    T++; // Создание массива
-    T += 3; // Инициализация
-    for (int i = 0, j = 0, k = 0; k < result.size(); k++) {
-        T++; // Сравнение
-        // Заполняем результат, выбирая меньший элемент из левого и правого массивов
-        result[k] = i < left.size() && (j == right.size() || left[i] < right[j]) ? left[i++] : right[j++];
-        T += 4;
-        T++; // k++
-        T++; // i++ or j++
-    }
-    T++; // Последнее сравнение
-    T++; // return
-    return result; // Возвращаем объединенный массив
-}
-
-// Функция сортировки слиянием
-std::vector<int> mergeSort(std::vector<int> arr, int& T) {
-    // Базовый случай: если массив содержит 1 или 0 элементов, он уже отсортирован
-    if (arr.size() == 1) {
-        T++; // return
-        return arr;
-    }
-    T++; // Сравнение
-
-    std::vector<int> left;  // Левый подмассив
-    std::vector<int> right; // Правый подмассив
-    T += 2; // Создание массивов
-    // Делим массив на два подмассива
-    T++; // Инициализация
-    for (int i = 0; i < arr.size(); i++) {
-        T += 2; // Сравнения
-        if (i < arr.size() / 2) {
-            left.push_back(arr[i]); // Добавляем элемент в левый подмассив
-            T++;
-        }
-        else {
-            right.push_back(arr[i]); // Добавляем элемент в правый подмассив
-            T++;
-        }
-
-        T++; // i++
-    }
-    T++; // Последнее сравение
-    T++; // return
-    // Рекурсивно сортируем левый и правый подмассивы, затем сливаем их
-    return merge(mergeSort(left, T), mergeSort(right, T), T);
-}
-
-void shellSort(vector<int>& arr, int& T) {
-
-}
-
-void quickSort(vector<int>& arr, int start, int end, int& T) {
-    T++; // Сравнение
-    if (end <= start) {
-        T++; // return
-        return;
-    }
-    int pivot = arr[end];
-    int i = start - 1;
-    T += 3;
-    for (int j = start; j < end; j++) {
-        T += 2; // Сравнения
-        if (arr[j] < pivot) {
-            i++;
-            int tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
-            T += 5;
-        }
-        T++; // j++
-    }
-    T++; // Последнее сравнение
-    i++;
-    int tmp = arr[i];
-    arr[i] = arr[end];
-    arr[end] = tmp;
-    int p_index = i;
-    T += 5;
-    quickSort(arr, start, p_index - 1, T);
-    quickSort(arr, p_index + 1, end, T);
-
-
-}
-
-
-
 
 
 // FILL
@@ -447,203 +273,275 @@ void fillDataFile() {
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         sortedFill(arr, n);
-        int T = 0;
-        selectionSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        selectionSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "SS;S;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         semiSortedFill(arr, n);
-        int T = 0;
-        selectionSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        selectionSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "SS;SS;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         unSortedFill(arr, n);
-        int T = 0;
-        selectionSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        selectionSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "SS;US;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         randomFill(arr, n);
-        int T = 0;
-        selectionSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        selectionSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "SS;RS;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         sortedFill(arr, n);
-        int T = 0;
-        insertionSort(arr, T);
+        
+        auto start = chrono::high_resolution_clock::now();
+        insertionSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "IS;S;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         semiSortedFill(arr, n);
-        int T = 0;
-        insertionSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        insertionSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "IS;SS;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         unSortedFill(arr, n);
-        int T = 0;
-        insertionSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        insertionSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "IS;US;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         randomFill(arr, n);
-        int T = 0;
-        insertionSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        insertionSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "IS;RS;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         sortedFill(arr, n);
-        int T = 0;
-        bubbleSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        bubbleSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "BS;S;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         semiSortedFill(arr, n);
-        int T = 0;
-        bubbleSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        bubbleSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "BS;SS;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         unSortedFill(arr, n);
-        int T = 0;
-        bubbleSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        bubbleSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "BS;US;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         randomFill(arr, n);
-        int T = 0;
-        bubbleSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        bubbleSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "BS;RS;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         sortedFill(arr, n);
-        int T = 0;
-        mergeSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        mergeSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "MS;S;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         semiSortedFill(arr, n);
-        int T = 0;
-        mergeSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        mergeSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "MS;SS;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         unSortedFill(arr, n);
-        int T = 0;
-        mergeSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        mergeSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "MS;US;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         randomFill(arr, n);
-        int T = 0;
-        mergeSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        mergeSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "MS;RS;" << n << "; " << T << endl;
     }
-    /*
+    
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         sortedFill(arr, n);
         int T = 0;
-        shellSort(arr, T);
+        shellSort(arr);
         data_file << "SHS;S;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         semiSortedFill(arr, n);
         int T = 0;
-        shellSort(arr, T);
+        shellSort(arr);
         data_file << "SHS;SS;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         unSortedFill(arr, n);
         int T = 0;
-        shellSort(arr, T);
+        shellSort(arr);
         data_file << "SHS;US;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         randomFill(arr, n);
         int T = 0;
-        shellSort(arr, T);
+        shellSort(arr);
         data_file << "SHS;RS;" << n << "; " << T << endl;
     }
-    */
+    
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
-        sortedFill(arr, n);
-        int T = 0;
-        quickSort(arr, 0, arr.size() - 1, T);
+        sortedFill(arr, 9000);
+        auto start = chrono::high_resolution_clock::now();
+        quickSort(arr, 0, arr.size() - 1);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "QS;S;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         semiSortedFill(arr, n);
-        int T = 0;
-        quickSort(arr, 0, arr.size() - 1, T);
+        auto start = chrono::high_resolution_clock::now();
+        quickSort(arr, 0, arr.size() - 1);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "QS;SS;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         unSortedFill(arr, n);
-        int T = 0;
-        quickSort(arr, 0, arr.size() - 1, T);
+        auto start = chrono::high_resolution_clock::now();
+        quickSort(arr, 0, arr.size() - 1);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "QS;US;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         randomFill(arr, n);
-        int T = 0;
-        quickSort(arr, 0, arr.size() - 1, T);
+        auto start = chrono::high_resolution_clock::now();
+        quickSort(arr, 0, arr.size() - 1);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "QS;RS;" << n << "; " << T << endl;
     }
-    /*
+
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         sortedFill(arr, n);
-        int T = 0;
-        heapSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        heapSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "HS;S;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         semiSortedFill(arr, n);
-        int T = 0;
-        heapSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        heapSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "HS;SS;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         unSortedFill(arr, n);
-        int T = 0;
-        heapSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        heapSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "HS;US;" << n << "; " << T << endl;
     }
     for (int n = FROM; n < TO; n += STEP) {
         vector<int> arr;
         randomFill(arr, n);
-        int T = 0;
-        heapSort(arr, T);
+        auto start = chrono::high_resolution_clock::now();
+        heapSort(arr);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        float T = duration.count();
         data_file << "HS;RS;" << n << "; " << T << endl;
     }
-    */
 
     
 
